d3e9badd873a29b2f32d201b2b232caab1e1e46b
dvoljatik <o.dvoljatik@cft.ru>
2020-11-25 20:14:07 +0700

    [init] Использовал библиотеку для навигации в Android

--- app/src/main/java/ru/endroad/rosatom/application/App.kt
+++ app/src/main/java/ru/endroad/rosatom/application/App.kt
@@ -1,5 +1,17 @@
 package ru.endroad.rosatom.application
 
 import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+import ru.endroad.rosatom.di.navigationModule
 
-class App : Application()
+class App : Application() {
+
+	override fun onCreate() {
+		super.onCreate()
+		startKoin {
+			androidContext(this@App)
+			modules(navigationModule)
+		}
+	}
+}
--- app/src/main/java/ru/endroad/rosatom/application/SingleActivity.kt
+++ app/src/main/java/ru/endroad/rosatom/application/SingleActivity.kt
@@ -1,10 +1,26 @@
 package ru.endroad.rosatom.application
 
+import android.os.Bundle
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.flow.collect
+import org.koin.android.ext.android.inject
 import ru.endroad.component.common.BaseActivity
+import ru.endroad.component.navigation.navigator.NavigationCommandStack
 import ru.endroad.rosatom.R
 
 class SingleActivity : BaseActivity() {
 
 	override val layout = R.layout.hub_activity
 	override val theme = R.style.ThemeRosatom
+
+	private val navigatorCommandStack: NavigationCommandStack by inject()
+
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		lifecycleScope.launchWhenResumed {
+			navigatorCommandStack.commandFlow.collect { requireNavigationContext ->
+				requireNavigationContext(baseContext, supportFragmentManager)
+			}
+		}
+	}
 }
