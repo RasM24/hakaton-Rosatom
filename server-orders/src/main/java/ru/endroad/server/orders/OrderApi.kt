fb9b05f6df17a5b8ac216fd1f9e5723c93b7af5f
dvoljatik <o.dvoljatik@cft.ru>
2020-11-27 23:04:50 +0700

    [order] Добавил источник заявок

---
+++ server-orders/src/main/java/ru/endroad/server/orders/OrderApi.kt
@@ -0,0 +1,4 @@
+package ru.endroad.server.orders
+
+interface OrderApi {
+}
---
+++ server-orders/src/main/java/ru/endroad/server/orders/data/OrderListDataSource.kt
@@ -0,0 +1,20 @@
+package ru.endroad.server.orders.data
+
+import ru.endroad.server.orders.OrderApi
+import ru.endroad.server.orders.entity.*
+
+class OrderListDataSource(
+	private val orderApi: OrderApi,
+) {
+
+	private val hardcode = listOf(
+		Order(1, 1, Performer.All, 0, 0, "title", Priority.CRITICAL, OrderType.COMMON, OrderStatus.InProgress, ""),
+		Order(1, 1, Performer.All, 0, 0, "title", Priority.HIGH, OrderType.COMMON, OrderStatus.Read, ""),
+		Order(1, 1, Performer.All, 0, 0, "title", Priority.LOW, OrderType.TECHNOLOGICAL, OrderStatus.InProgress, ""),
+		Order(1, 1, Performer.All, 0, 0, "title", Priority.HIGH, OrderType.COMMON, OrderStatus.New, ""),
+		Order(1, 1, Performer.All, 0, 0, "title", Priority.MEDIUM, OrderType.TECHNICAL, OrderStatus.InProgress, ""),
+	)
+
+	suspend fun getList(employerId: Long): List<Order> =
+		hardcode
+}
