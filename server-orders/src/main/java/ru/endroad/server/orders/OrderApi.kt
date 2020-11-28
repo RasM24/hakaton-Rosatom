42d300ba4120b0e59d0f46923e534859d1d647bc
dvoljatik <o.dvoljatik@cft.ru>
2020-11-28 21:11:38 +0700

    test

--- server-orders/src/main/java/ru/endroad/server/orders/OrderApi.kt
+++ server-orders/src/main/java/ru/endroad/server/orders/OrderApi.kt
@@ -1,4 +1,10 @@
 package ru.endroad.server.orders
 
+import retrofit2.http.GET
+import ru.endroad.server.orders.entity.Order
+
 interface OrderApi {
+
+	@GET("/orders")
+	suspend fun getOrders(): List<Order>
 }
