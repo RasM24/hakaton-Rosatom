e84df4ad2e183cf6a5906fb10403652abdeb7f2d
dvoljatik <o.dvoljatik@cft.ru>
2020-11-28 23:00:49 +0700

    [sensor] Добавил запуск пуш-уведомлений при критических значениях датчиков

--- app/src/main/java/ru/endroad/rosatom/di/RouterModule.kt
+++ app/src/main/java/ru/endroad/rosatom/di/RouterModule.kt
@@ -2,12 +2,11 @@ package ru.endroad.rosatom.di
 
 import org.koin.dsl.module
 import org.koin.experimental.builder.single
-import ru.endroad.rosatom.router.MainNavigationRouter
-import ru.endroad.rosatom.router.RootRouter
-import ru.endroad.rosatom.router.SummaryRouter
+import ru.endroad.rosatom.router.*
 
 val routerModule = module {
 	single<RootRouter>()
 	single<MainNavigationRouter>()
 	single<SummaryRouter>()
+	single<PushRouting>()
 }
--- app/src/main/java/ru/endroad/rosatom/view/monitoring/MonitoringViewModel.kt
+++ app/src/main/java/ru/endroad/rosatom/view/monitoring/MonitoringViewModel.kt
@@ -7,10 +7,16 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import ru.endroad.component.common.MviViewModel
 import ru.endroad.rosatom.data.SensorsDataSource
+import ru.endroad.rosatom.entity.SensorData
+import ru.endroad.rosatom.entity.isAlert
+import ru.endroad.rosatom.router.PushRouting
 
 class MonitoringViewModel(
 	private val sensorsDataSource: SensorsDataSource,
+	private val router: PushRouting,
 ) : ViewModel(), MviViewModel<MonitoringScreenState, MonitoringScreenEvent> {
+
+	private var alertSent = false
 
 	override val state = MutableStateFlow<MonitoringScreenState>(MonitoringScreenState.Initialized)
 
@@ -33,6 +39,17 @@ class MonitoringViewModel(
 
 	private suspend fun MonitoringScreenEvent.FetchData.reduce(): MonitoringScreenState {
 		val sensorDataList = sensorsDataSource.getList()
+
+		if (!alertSent && sensorDataList.any(SensorData::isAlert)) {
+			val message = sensorDataList
+				.find(SensorData::isAlert)
+				.let(::requireNotNull)
+				.let { "Критическое значение датчика ${it.type}: ${it.value}" }
+
+			router.sendNotification("Внимание!", message, "")
+			alertSent = true
+		}
+
 		return MonitoringScreenState.Data(sensorDataList)
 	}
 }
