6728b8c720e124e824d46e4d5b01b3f8bf3dfc54
dvoljatik <o.dvoljatik@cft.ru>
2020-11-29 11:29:33 +0700

    [order] Фикс удаления всех данных при записи

--- app/src/main/java/ru/endroad/rosatom/view/order/draft/DraftOrderScreenState.kt
+++ app/src/main/java/ru/endroad/rosatom/view/order/draft/DraftOrderScreenState.kt
@@ -5,6 +5,7 @@ import ru.endroad.rosatom.entity.DraftOrder
 sealed class DraftOrderScreenState {
 
 	object Initialized : DraftOrderScreenState()
+	object StopRecording : DraftOrderScreenState()
 	class RecognitionData(val draftOrder: DraftOrder) : DraftOrderScreenState()
 	object Recording : DraftOrderScreenState()
 }
